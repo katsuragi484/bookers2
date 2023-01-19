710c55d5cea883b5dacf7afcdf1110f94d267e23
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-01-19 11:43:53 +0000

    [Update]投稿画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  end
+
 end
