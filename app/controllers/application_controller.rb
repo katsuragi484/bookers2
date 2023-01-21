490bd79779b86fadc1ee72eff7de91ccf74c401f
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-01-21 00:05:41 +0000

    dd]Step2くらいまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters,if: :devise_controller?
 
   protected
@@ -8,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    new_user_session_path
+    root_path
   end
 
   def configure_permitted_parameters
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
+
   def edit
-    @book = Book.new
+    @book = Book.find(params[:id])
 
   end
 
@@ -13,11 +14,24 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-
+    @books = Book.all
+    @user = @book.user
   end
 
   def show
-    
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path
   end
 
   private
