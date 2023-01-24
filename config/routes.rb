99a8dcc968c55b30bc50e2d46975d39a53054604
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-01-24 02:50:55 +0000

    [modify]homesからhome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  root to: "home#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:index, :edit, :create, :show, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update]
-  get '/homes/about' => 'homes#about' , as: 'about'
+  get '/home/about' => 'home#about' , as: 'about'
   end
 
